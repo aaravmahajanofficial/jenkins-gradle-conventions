8915b774d7dc8917d93f651ee8b5736a090c3c2d
Aarav Mahajan <aaravmahajan2003@gmail.com>
2025-03-16 06:32:23 +0530

    add new module convention-plugin-build-logic

---
+++ convention-plugin-build-logic/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "convention-plugin-build-logic"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "jenkins-gradle-conventions"
-
+include("convention-plugin-build-logic")
